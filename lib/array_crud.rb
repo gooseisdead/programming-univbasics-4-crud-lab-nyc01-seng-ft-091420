c58c65b10ec822eacae69721a1e09fe8223b3923
Mike Smith <gooseisdead@mac.com>
2020-08-29 03:28:05 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -40,5 +40,6 @@ def retrieve_last_element_from_array(array)
 end
 
 def update_element_from_index(array, index_number, element)
-
+  array = ["wow", "gloves", "satchel", "fedora", "whip", "arrays!"]
+  array[4] = "totally"
 end

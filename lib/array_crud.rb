376db1a3d08acbf604c5353b89d005c3e30fc33b
Mike Smith <gooseisdead@mac.com>
2020-08-29 03:24:52 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -25,7 +25,8 @@ def remove_element_from_start_of_array(array)
 end
 
 def retrieve_element_from_index(array, index_number)
-
+  array = ["wow", "gloves", "am", "fedora", "whip", "arrays!"]
+  array[2]
 end
 
 def retrieve_first_element_from_array(array)
